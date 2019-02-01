21caf2f341bd017b2923328c84ad029ab5eace22
Klever-Sobrinho <kleverfortaleza@gmail.com>
2019-02-01 10:46:55 -0300

    Salve form data on MySQL

---
+++ src/main/java/com/springboot/demo/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springboot.demo.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String>{
+
+}
